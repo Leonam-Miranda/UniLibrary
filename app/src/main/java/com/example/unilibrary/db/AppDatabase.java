2d6a15996f5a141556e99cd3c957a86fd1ed54a4
Gabriel Xavier <gabriel2004407@gmail.com>
2026-05-23 19:00:21 -0300

    feat: add LoanDao, fix Loan entity with foreign keys and updateAppDataBase

--- app/src/main/java/com/example/unilibrary/db/AppDatabase.java
+++ app/src/main/java/com/example/unilibrary/db/AppDatabase.java
@@ -4,19 +4,23 @@ import android.content.Context;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.room.Database;
+import androidx.room.TypeConverters;
 
-
+import com.example.unilibrary.db.Converters;
 import com.example.unilibrary.db.dao.BookDao;
+import com.example.unilibrary.db.dao.LoanDao;
 import com.example.unilibrary.db.dao.UserDao;
 import com.example.unilibrary.model.Book;
 import com.example.unilibrary.model.Loan;
 import com.example.unilibrary.model.User;
 
 @Database(entities = {User.class, Book.class, Loan.class}, version = 1, exportSchema = false)
+@TypeConverters(Converters.class)
 public abstract class AppDatabase extends RoomDatabase {
     private static volatile AppDatabase instance;
     public abstract UserDao userDao();
     public abstract BookDao bookDao();
+    public abstract LoanDao loanDao();
     public static AppDatabase getInstance(Context ctx) {
         if (instance == null) {
             synchronized (AppDatabase.class) {
--- app/src/main/java/com/example/unilibrary/db/Converters.java
+++ app/src/main/java/com/example/unilibrary/db/Converters.java
@@ -4,6 +4,8 @@ import androidx.room.TypeConverter;
 
 import com.example.unilibrary.enums.BookStatus;
 
+import java.util.Date;
+
 public class Converters {
 
     @TypeConverter
@@ -14,5 +16,15 @@ public class Converters {
     @TypeConverter
     public static BookStatus toBookStatus(String value){
         return value == null ? null : BookStatus.valueOf(value);
+    }
+
+    @TypeConverter
+    public static Long fromDate(Date date) {
+        return date == null ? null : date.getTime(); // Date → número pra salvar
+    }
+
+    @TypeConverter
+    public static Date toDate(Long value) {
+        return value == null ? null : new Date(value); // número → Date ao ler
     }
 }
